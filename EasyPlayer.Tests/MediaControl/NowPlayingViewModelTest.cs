ea129e86b636cc900ed9dd90635c2d92349350b5
Andy Whitfield <andy@andywhitfield.me.uk>
2011-06-13 18:41:39 +0100

    Added tests for Shell and NowPlaying.

---
+++ EasyPlayer.Tests/MediaControl/NowPlayingViewModelTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Caliburn.Micro;
+using EasyPlayer.Library;
+using EasyPlayer.MediaControl;
+using EasyPlayer.Messages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace EasyPlayer.Tests.MediaControl
+{
+    [TestClass]
+    public class NowPlayingViewModelTest
+    {
+        [TestMethod]
+        public void Given_a_play_request_message_should_stop_currently_playing_item_and_start_new_media()
+        {
+            var eventAgg = new Mock<IEventAggregator>();
+            var media = new Mock<IMediaItem>();
+            var dummyStream = new MemoryStream();
+            media.Setup(x => x.Name).Returns("Fake Media Item");
+            media.Setup(x => x.DataStream).Returns(dummyStream);
+            var vm = new NowPlayingViewModel(eventAgg.Object);
+
+            var capturedEvents = new List<string>();
+            vm.PropertyChanged += (s, e) => capturedEvents.Add(e.PropertyName);
+            vm.Handle(new PlayRequestMessage(media.Object));
+
+            var playStateChanges = capturedEvents.Where(x => x == "MediaPlayerState");
+            Assert.AreEqual(2, playStateChanges.Count());
+            Assert.AreEqual(PlayerState.Playing, vm.MediaPlayerState);
+            Assert.AreSame(dummyStream, vm.MediaStream);
+            Assert.AreEqual("Fake Media Item", vm.CurrentlyPlaying);
+        }
+
+        [TestMethod]
+        public void When_playing_then_playpause_should_be_pause_otherwise_should_be_play()
+        {
+            var eventAgg = new Mock<IEventAggregator>();
+            var vm = new NowPlayingViewModel(eventAgg.Object);
+
+            vm.MediaPlayerState = PlayerState.Stopped;
+            Assert.AreEqual("Play", vm.PlayPauseText);
+
+            vm.MediaPlayerState = PlayerState.Paused;
+            Assert.AreEqual("Play", vm.PlayPauseText);
+
+            vm.MediaPlayerState = PlayerState.Playing;
+            Assert.AreEqual("Pause", vm.PlayPauseText);
+        }
+    }
+}
---
+++ EasyPlayer.Tests/ShellViewModelTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Caliburn.Micro;
+using EasyPlayer.Shell;
+using EasyPlayer.Widgets;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace EasyPlayer.Tests
+{
+    [TestClass]
+    public class ShellViewModelTest
+    {
+        [TestMethod]
+        public void When_nowplaying_is_visible_then_active_item_should_not_be_visible()
+        {
+            var widgets = new Mock<IAppWidget>();
+            var eventAgg = new Mock<IEventAggregator>();
+            var vm = new ShellViewModel(new[] { widgets.Object }, eventAgg.Object);
+
+            Assert.IsFalse(vm.NowPlayingVisible);
+            Assert.IsTrue(vm.ActiveItemVisible);
+
+            var capturedEvents = new List<string>();
+            vm.PropertyChanged += (s, e) => capturedEvents.Add(e.PropertyName);
+            vm.NowPlayingVisible = true;
+
+            Assert.IsTrue(vm.NowPlayingVisible);
+            Assert.IsFalse(vm.ActiveItemVisible);
+            Assert.AreEqual(2, capturedEvents.Count);
+            Assert.IsTrue(capturedEvents.Contains("NowPlayingVisible"));
+            Assert.IsTrue(capturedEvents.Contains("ActiveItemVisible"));
+        }
+    }
+}
